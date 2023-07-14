e6b7896b99c543e8fedceb768bc0b5600c745487
dawgselmo15 <dawgselmo15@gmail.com>
2023-07-14 09:34:59 +0800

    lts

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,12 @@
+import {Navbar,Container, Nav} from "react-bootstrap";
+
+function NavigationBar(){
+  return(
+    <div>
+      <h1>Navigasi TESTING</h1>
+    </div>
+  );
+}
+
+
+export default NavigationBar
